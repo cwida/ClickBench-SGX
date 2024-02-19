dcd431a9b28a567665617044b63e25c5cccdeccb
ila <ilaria.battiston@gmail.com>
2024-02-18 18:46:12 -0800

    fixed parquet bugs

--- duckdb-parquet/load.py
+++ duckdb-parquet/load.py
@@ -16,9 +16,9 @@ con.execute("SET preserve_insertion_order=false")
 print("Will load the data")
 start = timeit.default_timer()
 con.execute(open("duckdb-parquet/create.sql").read())
-con.execute("COPY hits FROM 'duckdb-parquet/hits.parquet'")
-con.execute("PRAGMA add_parquet_key('key128', '0123456789112345'");
+con.execute("PRAGMA add_parquet_key('key128', '0123456789112345')");
 print("Will export the encrypted data")
-con.execute("COPY hits TO 'hits_encrypted.parquet' (ENCRYPTION_CONFIG {footer_key: 'key128'})")
+con.execute("COPY hits TO 'duckdb-parquet/hits_encrypted.parquet' (ENCRYPTION_CONFIG {footer_key: 'key128'})")
+con.execute("DROP TABLE hits")
 end = timeit.default_timer()
 print(end - start)
