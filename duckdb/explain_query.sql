19d46fd3228e0bd959b75e73b18fa455275c5900
ila <ilaria.battiston@gmail.com>
2024-02-26 01:27:44 -0800

    more benchmarks

--- duckdb/explain_query.sql
+++ duckdb/explain_query.sql
@@ -1,5 +1,1 @@
 SELECT WatchID, ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID, ClientIP ORDER BY c DESC LIMIT 10;
-SELECT WatchID, ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID, ClientIP ORDER BY c LIMIT 10;
-SELECT WatchID, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID ORDER BY c LIMIT 10;
-SELECT ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY ClientIP ORDER BY c LIMIT 10;
-SELECT RegionID, SUM(AdvEngineID), COUNT(*) AS c, AVG(ResolutionWidth), COUNT(DISTINCT UserID) FROM hits GROUP BY RegionID ORDER BY c DESC LIMIT 10;
