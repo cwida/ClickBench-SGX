87a4ba7b0d39488bbed6aa45b8ba5f55a25ea136
ila <ilaria.battiston@gmail.com>
2024-02-25 22:41:44 -0800

    added more results

--- duckdb/explain_query.sql
+++ duckdb/explain_query.sql
@@ -1,1 +1,5 @@
-SELECT * FROM read_parquet('duckdb-parquet-encrypted/hits_encrypted.parquet', encryption_config={footer_key: 'key128'}) WHERE URL LIKE '%google%' ORDER BY EventTime LIMIT 10;
+SELECT WatchID, ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID, ClientIP ORDER BY c DESC LIMIT 10;
+SELECT WatchID, ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID, ClientIP ORDER BY c LIMIT 10;
+SELECT WatchID, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY WatchID ORDER BY c LIMIT 10;
+SELECT ClientIP, COUNT(*) AS c, SUM(IsRefresh), AVG(ResolutionWidth) FROM hits GROUP BY ClientIP ORDER BY c LIMIT 10;
+SELECT RegionID, SUM(AdvEngineID), COUNT(*) AS c, AVG(ResolutionWidth), COUNT(DISTINCT UserID) FROM hits GROUP BY RegionID ORDER BY c DESC LIMIT 10;
--- duckdb/profile_duckdb.py
+++ duckdb/profile_duckdb.py
@@ -4,26 +4,33 @@ import os
 import duckdb
 import timeit
 
-def run_query(query):
-    print(f"\nRunning query: {query.strip()}", flush=True)
+def run_query(query, explain_file):
+    with open(explain_file, 'a') as log:
+        print(f"\nRunning query: {query.strip()}", flush=True)
+        log.write(query + '\n')
 
-    con = duckdb.connect(database="duckdb/my-db.duckdb", read_only=False)
-    con.execute("PRAGMA enable_profiling")
-    con.execute("PRAGMA add_parquet_key('key128', '0123456789112345')")
-    
-    for try_num in range(3):
-        start = timeit.default_timer()
-        con.sql(query).explain("analyze")
-        end = timeit.default_timer()
-        print(f"Query finished in {end - start:.6f} seconds", flush=True)
+        con = duckdb.connect(database="duckdb/my-db.duckdb", read_only=False)
+        con.execute("PRAGMA enable_profiling")
+        con.execute("PRAGMA add_parquet_key('key128', '0123456789112345')")
+        
+        for try_num in range(3):
+            start = timeit.default_timer()
+            con.sql(query).explain("analyze")
+            end = timeit.default_timer()
+            log.write(str(end - start) + '\n')
+            print(f"Query finished in {end - start:.6f} seconds", flush=True)
 
 def main():
     queries_file_path = os.path.join(os.getcwd(), "duckdb/explain_query.sql")
+    explain_file_path = os.path.join(os.getcwd(), "duckdb/explain.txt")
     print("Starting benchmark...", flush=True)
+
+    if os.path.exists(explain_file_path):
+        os.remove(explain_file_path)
 
     with open(queries_file_path, 'r') as queries_file:
         for query in queries_file:
-            run_query(query.strip())
+            run_query(query.strip(), explain_file_path)
 
     print("\nBenchmark finished. Check duckdb/explain.txt for the query timings.", flush=True)
 
