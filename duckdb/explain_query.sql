b672897e78df1633426098a465b9045d02b7bd15
ila <ilaria.battiston@gmail.com>
2024-03-11 11:39:14 +0100

    updated manifest

--- duckdb/explain_query.sql
+++ duckdb/explain_query.sql
@@ -1,1 +1,1 @@
-SELECT * FROM read_parquet('duckdb-parquet-encrypted/hits_encrypted.parquet', encryption_config={footer_key: 'key128'}, force_direct_io=true) WHERE URL LIKE '%google%' ORDER BY EventTime LIMIT 10;
+SELECT UserID FROM read_parquet('duckdb-parquet-encrypted/hits.parquet', force_direct_io=true);
